ee7954dc94454b161489231678930101f13e0129
Hemanth-Kumar-KH3 <hemanthkh3123@gmail.com>
2026-04-07 14:17:09 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import requests
 import os
 from customer_support_env.server.customer_support_env_environment import CustomerSupportEnvironment
 
-API_URL = "https://router.huggingface.co/hf-inference/v1/models/google/flan-t5-base"
+API_URL = "https://router.huggingface.co/hf-inference/v1"
 MODEL_NAME=os.getenv('MODEL_NAME')
 headers = {
     "Authorization": f"Bearer {os.getenv('HF_TOKEN')}",
